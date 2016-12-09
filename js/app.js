ad7ccb6977f93548dd1b3b9f8fc35d4aacd763c0
nikolaykolibarov <nikolaykolibarov@gmail.com>
2016-12-09 06:45:15 +0200

    Refactored code. Added Home markup

--- js/app.js
+++ js/app.js
@@ -6,9 +6,10 @@
 
     const appKey = 'kid_SJFQs5PXl';
     const appSecret = 'kid_SJFQs5PXl';
+    const baseServiceUrl = 'https://baas.kinvey.com/';
 
     let router = Sammy(function () {
-        let AuthorizationService = new AuthorizationService(appKey, appSecret, 'https://baas.kinvey.com/');
+        let AuthorizationService = new AuthorizationService(appKey, appSecret, baseServiceUrl);
         let Requester = new Requester(AuthorizationService);
 
         this.get('#/', function () {
